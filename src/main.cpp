3debab848b9234dacdb224444f38fda38fda3875
echo-devim <devim@outlook.it>
2020-03-02 20:16:34 +0100

    Launch a new istance for each argument passed on command line

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,19 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QString filepath = "";
-    if ((argc > 1) && (QFile::exists(argv[1]))) {
-        filepath = argv[1];
-    }
-    Fhex w(nullptr, &a, filepath);
 
-    w.show();
+    if (argc > 1) {
+        for (int i = 0; i < argc; i++) {
+            if (QFile::exists(argv[i])) {
+                filepath = argv[i];
+                Fhex *window = new Fhex(nullptr, &a, filepath);
+                window->show();
+            }
+        }
+    } else {
+        Fhex *window = new Fhex(nullptr, &a);
+        window->show();
+    }
 
     return a.exec();
 }
