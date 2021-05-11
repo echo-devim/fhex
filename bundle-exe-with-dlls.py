92e52dfbebfeb71a9593ca706d5f7bcabf0140b9
Thomas P <TPXP@users.noreply.github.com>
2021-05-11 22:35:16 +0200

    CI: Fix build for Fhex 3

--- bundle-exe-with-dlls.py
+++ bundle-exe-with-dlls.py
@@ -5,7 +5,10 @@ from shutil import copyfile
 # Determine the destination directory
 dest_dir="fhex-{}-{}".format(time.strftime("%Y%m%d%H%M%S"), os.environ['GITHUB_SHA'])
 # Set it as an environment variable (for the next steps)
-print("::set-env name=BUNDLE_DIR::{}".format(dest_dir))
+with open(os.environ['GITHUB_ENV'], 'a') as f:
+	f.writelines([
+		"BUNDLE_DIR=%s" % dest_dir,
+	])
 
 os.mkdir(dest_dir)
 
@@ -25,8 +28,11 @@ for file in os.listdir(dest_dir):
 		if dep[-4:] != ".dll":
 			continue
 		# Find the DLL
-		dll_path = os.popen("where {}".format(dep)).readlines()
-		dll_path = dll_path[0].strip()
+		dll_path = 'lib/%s' % dep
+		# If not found in our custom lib directory, ask the system
+		if not os.path.isfile(dll_path):
+			dll_path = os.popen("where {}".format(dep)).readlines()
+			dll_path = dll_path[0].strip()
 		# If it's in system 32, it's safe to assume it's provided by Windows
 		if dll_path.lower().startswith("c:\\windows\\system32"):
 			print("Skipping {}", dll_path)
